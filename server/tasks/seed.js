3b55068048a3a845d86e9b4836c7cdf580b4843c
rgorai <43449685+rgorai@users.noreply.github.com>
2021-11-15 16:27:30 -0500

    added json of movies

--- server/tasks/seed.js
+++ server/tasks/seed.js
@@ -1,13 +1,31 @@
 const connection = require('../config/mongoConnection')
 const axios = require('axios')
+const allMovies = require('./movie_ids_11_14_2021.json')
+
 const { create: createMovie } = require('../data/movies')
 
 const tmdbApiKey = '?api_key=31cc954c3de9a91aecd102e07e4d4707'
 const tmbdUrl = 'https://api.themoviedb.org/3/movie/'
 
+const NUM_GENRES = 10   // number of genres to gather
+const NUM_MOVIES = 20   // number of movies per genre
+
 const getMovie = async (movieId) => {
   try {
-    const { data } = await axios.get(tmbdUrl + movieId + tmdbApiKey)
+    const { data } = await axios.get(
+      tmbdUrl + movieId + tmdbApiKey
+    )
+    return data
+  } catch(e) {
+    console.error(e)
+  }
+}
+
+const getMovieProviders = async (movieId) => {
+  try {
+    const { data } = await axios.get(
+      tmbdUrl + movieId + '/watch/providers' + tmdbApiKey
+    )
     return data
   } catch(e) {
     console.error(e)
@@ -18,8 +36,21 @@ const main = async () => {
   const db = await connection.connectToDb()
   await db.dropDatabase()
 
-  console.log(await getMovie('14607'))
-  
+  // gather movie data
+  const moviesToSeed = []
+  let foundGenres = []
+  while (foundGenres.length < NUM_GENRES) {
+    // get random movie id
+    const randomMovieId = allMovies[Math.floor(Math.random() * allMovies.length)].id
+
+    // check genre
+    const randomMovie = await getMovie('1726')
+    console.log(randomMovie)
+    const { results } = await getMovieProviders('1726')
+    console.log(results.US.rent)
+    break
+  }
+
   console.log('Done seeding database');
   connection.closeConnection()
 }
