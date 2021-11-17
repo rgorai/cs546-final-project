6ee5c2e86927ae352d71d3930f839ef7354fdcf6
rgorai <43449685+rgorai@users.noreply.github.com>
2021-11-17 14:57:26 -0500

    navbar

--- client/src/components/home/HomePage.js
+++ client/src/components/home/HomePage.js
@@ -1,5 +1,6 @@
 // import { useEffect, useState } from 'react'
 import NewUserForm from '../users/NewUserForm'
+import './homePage.css'
 
 const HomePage = (props) => {
   
--- client/src/components/home/NavBar.js
+++ client/src/components/home/NavBar.js
@@ -1,10 +1,13 @@
 import { Link } from 'react-router-dom'
-import './homePage.css'
+import './navBar.css'
 
 const NavBar = (props) => {
   
   return (
     <nav className="nav-bar-container">
+      <div className="website">
+        <Link to="/">MovieHub</Link>
+      </div>
       <ul className="nav-bar">
         <li><Link to="/">Home</Link></li>
         <li><Link to="/movies">Movies</Link></li>
