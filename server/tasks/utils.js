2c7fdbdf88086db05c4c9d0ebf373b4315efedf8
rgorai <43449685+rgorai@users.noreply.github.com>
2021-11-27 13:53:28 -0500

    updated?

--- server/tasks/utils.js
+++ server/tasks/utils.js
@@ -29,45 +29,45 @@ const tmdbRequest = async (url) => {
   try {
     const { data } = await axios.get(url)
     return data
-  } catch(e) {
-    throw 'TMDB '+ String(e)
+  } catch (e) {
+    throw 'TMDB ' + String(e)
   }
 }
 
 const getMediaData = async (
   numMedia,
-  allMedia, 
-  mediaReqs, 
-  getMedia, 
+  allMedia,
+  mediaReqs,
+  getMedia,
   getMediaProviders,
   propsToAdd
-  ) => {
+) => {
   const media = []
 
   for (let i = 0; i < numMedia; i++) {
     const mediaId = allMedia[Math.floor(Math.random() * allMedia.length)].id
     const mediaRes = await getMedia(mediaId)
-    
+
     // check reqs
     let skip = false
     for (const k of Object.keys(mediaReqs))
-      if (k !== 'providers' && (!mediaRes[k] || 
-          ((typeof(mediaRes[k]) === 'string' || Array.isArray(mediaRes[k]))
-            && mediaRes[k].length === 0)))
-        if (mediaReqs[k] > 0)
-          mediaReqs[k]--
+      if (
+        k !== 'providers' &&
+        (!mediaRes[k] ||
+          ((typeof mediaRes[k] === 'string' || Array.isArray(mediaRes[k])) &&
+            mediaRes[k].length === 0))
+      )
+        if (mediaReqs[k] > 0) mediaReqs[k]--
         else {
           i--
           skip = true
           break
         }
-    if (skip)
-      continue
+    if (skip) continue
 
     const mediaProviders = await getMediaProviders(mediaId)
     if (!mediaProviders.results.US)
-      if (mediaReqs.providers > 0)
-        mediaReqs.providers--
+      if (mediaReqs.providers > 0) mediaReqs.providers--
       else {
         i--
         continue
@@ -75,7 +75,7 @@ const getMediaData = async (
 
     // add media to return
     media.push(propsToAdd.map((k) => mediaRes[k]))
-    media[media.length-1].push(mediaProviders.results.US)
+    media[media.length - 1].push(mediaProviders.results.US)
 
     process.stdout.write('|')
   }
@@ -84,31 +84,45 @@ const getMediaData = async (
 }
 
 const getMovieData = async (numMedia, mediaReqs) => {
-  return await getMediaData(numMedia, allMovies, mediaReqs, getMovie, getMovieProviders, [
-    'original_title',
-    'release_date',
-    'release_dates',
-    'runtime',
-    'genres',
-    'overview',
-    'poster_path',
-    'videos',
-  ])
+  return await getMediaData(
+    numMedia,
+    allMovies,
+    mediaReqs,
+    getMovie,
+    getMovieProviders,
+    [
+      'original_title',
+      'release_date',
+      'release_dates',
+      'runtime',
+      'genres',
+      'overview',
+      'poster_path',
+      'videos',
+    ]
+  )
 }
 
 const getShowData = async (numMedia, mediaReqs) => {
-  return await getMediaData(numMedia, allShows, mediaReqs, getShow, getShowProviders, [
-    'name',
-    'first_air_date',
-    'overview',
-    'number_of_seasons',
-    'number_of_episodes',
-    'genres',
-    'poster_path',
-  ])
+  return await getMediaData(
+    numMedia,
+    allShows,
+    mediaReqs,
+    getShow,
+    getShowProviders,
+    [
+      'name',
+      'first_air_date',
+      'overview',
+      'number_of_seasons',
+      'number_of_episodes',
+      'genres',
+      'poster_path',
+    ]
+  )
 }
 
 module.exports = {
   getMovieData,
-  getShowData
+  getShowData,
 }
