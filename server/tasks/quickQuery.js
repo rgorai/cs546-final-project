93688fa4bc7302fab06fe01056479b6250ed5b46
rgorai <43449685+rgorai@users.noreply.github.com>
2021-11-27 12:23:52 -0500

    testing

--- server/tasks/quickQuery.js
+++ server/tasks/quickQuery.js
@@ -4,14 +4,13 @@ const NUM_MEDIA = 10
 
 // think about including backdrop on moviepage
 
-
 const main = async () => {
   // get movie data
-  console.log(await getMovieData(NUM_MEDIA))
+  console.log(await getMovieData(NUM_MEDIA));
 
   // get show data
 }
 
 main().catch((e) => {
-  console.error(e)
+  console.error(e);
 })
