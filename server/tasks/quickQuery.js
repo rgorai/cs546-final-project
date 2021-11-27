1eaaf1df0a8a83e804cf06c0251dfe72fc134913
rgorai <43449685+rgorai@users.noreply.github.com>
2021-11-27 12:52:12 -0500

    testing

--- server/tasks/quickQuery.js
+++ server/tasks/quickQuery.js
@@ -1,16 +1,17 @@
-const { getMovieData } = require("./utils");
+const { getMovieData } = require('./utils')
 
-const NUM_MEDIA = 10;
+const NUM_MEDIA = 10
 
 // think about including backdrop on moviepage
 
 const main = async () => {
-  // get movie data
-  console.log(await getMovieData(NUM_MEDIA));
+    // get movie data
+    console.log(await getMovieData(NUM_MEDIA))
 
-  // get show data
-};
+    // get show data
+    const s = 'hello'
+}
 
 main().catch((e) => {
-  console.error(e);
-});
+    console.error(e)
+})
