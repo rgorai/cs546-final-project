829cfc9c2c6a1f3484b21c4a4770ec9c4c7b60f9
Jaiganesh Rao <jai.31rao@gmail.com>
2021-12-02 15:41:36 -0500

    working on home page trending media

--- client/src/components/home/HomePage.js
+++ client/src/components/home/HomePage.js
@@ -1,13 +1,33 @@
-// import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import '../../styles/home/homePage.css'
 
+import MovieList from '../movies/MovieList'
+import ShowList from '../shows/ShowList'
+
+import '../../styles/movies/movieList.css'
+import '../../styles/shows/showList.css'
+
 const HomePage = (props) => {
+  
+
+
   return (
     <div className="home-page-container">
       <h1>Welcome to MediaHub</h1>
-      <p>Your Streaming Guide for Movies and TV Shows</p>
+      <h2>Your Streaming Guide for Movies and TV Shows</h2>
+      <label>
+        <h3>Now Trending</h3>
+      </label>
+      <label>
+        <h4>Movies</h4>
+      </label>
+      <label>
+        <h4>Shows</h4>
+      </label>
+      
     </div>
+    
   )
 }
 
