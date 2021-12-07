f2a356f3de115ba158982d62c7930b0dadf30842
Sushant <sush1393@gmail.com>
2021-12-07 17:38:57 -0500

    display user watchlist and reviews on profile page

--- client/src/components/users/UserProfile.js
+++ client/src/components/users/UserProfile.js
@@ -18,45 +18,52 @@ const UserProfile = (props) => {
       .catch((e) => setError(e.response))
   }, [])
 
-  // const getWatchlist = () => {
-  //   console.log(user)
-  //   if (user.watchlist.length === 0)
-  //     return <li>No Watchlist</li>
-  //   return <li>in the get watchlist function</li>
-  // }
-
-  // const getReviews = () => {
-  //   console.log(user)
-  //   if (user.reviews.length === 0)
-  //     return <li>No Reviews</li>
-  //   user.reviews.forEach(review => {
-  //     <ul>
-  //       <li>review.reviewer</li>
-  //       <li>review.dateOfReview</li>
-  //       <li>review.review</li>
-  //       <li>review.like_dislike</li>
-  //     </ul>
-  //   })
-  // }
-
   return (
     <div className="profile-container">
       {error ? (
         <ApiError error={error} />
       ) : user ? (
         <div className="user-page-container">
-          <div>
-            [User Info Here (watchlist, reviews, liked movies?)]
-            {/* <p>Watchlist</p> */}
-            {/* <div className="watchlist">{getWatchlist()}</div> */}
-            {/* <ul className="reviews">{getReviews()}</ul> */}
-            <ul className="watchlist">
-              {user.watchlist.length === 0 ? (
-                <li>N/A</li>
-              ) : (
-                user.watchlist.map((item) => <li>{item}</li>)
-              )}
-            </ul>
+          <label htmlFor="" className="desc-bold heading">
+            My Watchlist :
+          </label>
+          <ul className="watchlist">
+            {user.watchlist.length === 0 ? (
+              <li>You haven't created any watchlist yet.</li>
+            ) : (
+              user.watchlist.map((item) => <li>{item}</li>)
+            )}
+          </ul>
+          <div className="user-reviews">
+            <label htmlFor="" className="desc-bold heading">
+              My Reviews :
+            </label>
+            {user.reviews.length === 0 ? (
+              <p>You haven't reviewed any entertainment programmes yet.</p>
+            ) : (
+              user.reviews.map((item) => (
+                <div>
+                  <p>
+                    <label htmlFor="" className="desc-bold">
+                      Movie / Show Name
+                    </label>
+                  </p>
+                  <p>
+                    <label htmlFor="">Date of Review: </label>{' '}
+                    {item.dateOfReview}
+                  </p>
+                  <p>
+                    <label htmlFor="">Liked / Disliked : </label>{' '}
+                    {item.like_dislike === 1 ? 'Liked' : 'Disliked'}
+                  </p>
+                  <p>
+                    {' '}
+                    Review:
+                    {item.review}
+                  </p>
+                </div>
+              ))
+            )}
           </div>
           <button onClick={() => navigate('/profile/edit')}>
             Edit Profile
